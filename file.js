dc92307b066179437b5ec66926140c0a71076c95
SyedFazal110 <syedfazal808@gmail.com>
2024-03-26 23:13:47 +0500

    js file created

---
+++ file.js
@@ -0,0 +1,47 @@
+// Qno No:01
+// let a = "Hello World";
+// for (let a = 1; a <= 5; a++) {
+//     document.write(a ," ", "Hello World<br>");
+// }
+
+
+
+
+// Qno No:02
+// let a = 10;
+// for (let a = 1; a <= 10; a++) {
+//     document.write(a , "<br>");
+// }
+
+
+
+
+// Qno No:03
+// let userInput = +prompt("Enter a number");
+// // const times = +prompt("How many times");
+
+// for(let i = 1; i <= 10; i++){
+//     document.write(`${userInput} * ${i} = ${userInput*i}</br>`)
+// }
+
+
+
+
+// Qno No:04
+// let name = ["Syed", "Fazal", "Ali", "Jafferi"];
+// let ul = document.querySelector('ul')
+// for (let i =0;i < name.length; i++) {
+//    ul.innerHTML +=`<li>${name[i]}</li>`
+// }  
+
+
+
+
+// Qno No:05
+// let fruit = ["apple" , "banana" , "pineapple" , "mango" , "orange"];
+// let ol = document.querySelector("ol");
+// let para = document.querySelector("#para");
+// for (let i = 0; i < fruit.length; i++) {
+//     ol.innerHTML += `<li>${fruit[i]}</li>`
+//     para.innerHTML +=`<li>Element at index ${i} is ${fruit[i]}<br></li>`
+// }
